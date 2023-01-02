55d29fbc6b94d52517fe32e0607bac41ce3a275e
jiepengtan <jiepengtan@gmail.com>
2023-01-02 20:14:21 +0800

    [add] 增加对 LVector2Int，LVector3Int 的Editor 支持

---
+++ Editor/EditorLVector2Int.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using Lockstep.Math;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(LVector2Int))]
+public class EditorLVector2Int : UnityEditor.PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label){
+        var xProperty = property.FindPropertyRelative("x");
+        var yProperty = property.FindPropertyRelative("y");
+        float LabelWidth = EditorGUIUtility.labelWidth - EditorLVectorDrawTool.LableWidthOffset;
+        float lableWid = EditorLVectorDrawTool.LableWid;
+
+        var labelRect = new Rect(position.x, position.y, LabelWidth, position.height);
+        EditorGUI.LabelField(labelRect, label);
+        float filedWid = (position.width - LabelWidth) / 2 - lableWid;
+        float initX = position.x + LabelWidth;
+        float offset = 0;
+        EditorLVectorDrawTool.DrawFieldInt(position, initX, ref offset, lableWid, filedWid, xProperty, new GUIContent("x:"));
+        EditorLVectorDrawTool.DrawFieldInt(position, initX, ref offset, lableWid, filedWid, yProperty, new GUIContent("y:"));
+    }
+}
+#endif
---
+++ Editor/EditorLVector3Int.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+using Lockstep.Math;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(LVector3Int))]
+public class EditorLVector3Int : UnityEditor.PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label){
+        var xProperty = property.FindPropertyRelative("x");
+        var yProperty = property.FindPropertyRelative("y");
+        var zProperty = property.FindPropertyRelative("z");
+        float LabelWidth = EditorGUIUtility.labelWidth - EditorLVectorDrawTool.LableWidthOffset;
+        float lableWid = EditorLVectorDrawTool.LableWid;
+
+        var labelRect = new Rect(position.x, position.y, LabelWidth, position.height);
+        EditorGUI.LabelField(labelRect, label);
+        float filedWid = (position.width - LabelWidth) / 3 - lableWid;
+        float initX = position.x + LabelWidth;
+        float offset = 0;
+        EditorLVectorDrawTool.DrawFieldInt(position, initX, ref offset, lableWid, filedWid, xProperty, new GUIContent("x:"));
+        EditorLVectorDrawTool.DrawFieldInt(position, initX, ref offset, lableWid, filedWid, yProperty, new GUIContent("y:"));
+        EditorLVectorDrawTool.DrawFieldInt(position, initX, ref offset, lableWid, filedWid, zProperty, new GUIContent("z:"));
+    }
+}
+#endif
